ca7766127b8e402704b52ad6c87165d264289791
IT-seoyun <imseoyun45@gmail.com>
2024-09-10 10:34:54 +0900

    프로그래밍 실습 1~2주치

---
+++ 004/004.cpp
@@ -0,0 +1,32 @@
+﻿// 004.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'A';//문자는 따옴표
+    char str[20]="Hello Word";//문자열은 큰따옴표
+    float flt = 10.234;
+    int no = 150;
+    double dbl = 20.123456;
+
+    printf("Character is %c\n", ch);
+    printf("String is %s\n", str);
+    printf("floating Value is %f\n", flt);
+    printf("Integer Value is %d\n", no);
+    printf("Double Value is %f\n", dbl);
+    printf("Octal Value is %o\n", no);
+    printf("Hexadecimal Value is %x\n", no);
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
